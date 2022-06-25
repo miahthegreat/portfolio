2309b7713274ca3f41ae6d53657736d6d75cbf0e
miahthegreat <j.j.schmid85@gmail.com>
2022-06-24 20:15:14 -0400

    updated project card border colors

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -3,7 +3,13 @@ import React from "react";
 const ProjectCard = ({ project }) => {
   const { title, description, demo, repo, color } = project;
   return (
-    <div className={`rounded-md bg-gradient-to-r from-sky-300 to-sky-600 p-1`}>
+    <div
+      className={`rounded-md bg-gradient-to-r ${
+        color === "red" ? "from-red-300 to-red-600" : ""
+      } ${color === "sky" ? "from-blue-300 to-blue-600" : ""} ${
+        color === "green" ? "from-green-300 to-green-600" : ""
+      } p-[.05rem]`}
+    >
       <div className="flex h-full flex-col justify-between gap-6 rounded-md bg-black p-2 text-gray-50">
         <div>
           <h3 className="text-base font-bold">{title}</h3>
