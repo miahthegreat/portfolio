cf353541cc25182657b5953881c45d3dc9271e17
miahthegreat <j.j.schmid85@gmail.com>
2023-06-10 16:31:09 -0400

    really escaped apostrophes this tme

--- components/Hero.js
+++ components/Hero.js
@@ -15,7 +15,7 @@ const Hero = () => {
         </div>
         <div>
           <h1 className="text-white font-black lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2">
-            Hi, I'm <span className="text-[#4d7887]">Jeremiah</span>
+            Hi, I&apos;m <span className="text-[#4d7887]">Jeremiah</span>
           </h1>
           <p className="mt-2 text-white-100 font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]">
             I develop 3D visuals, user&nbsp;
